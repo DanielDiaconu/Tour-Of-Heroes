eb1cdb4a0e73faeb96dbc40940dd26580fe30a48
Daniel Diaconu <daniel.diaconu234@gmail.com>
2021-11-24 21:38:12 +0200

    step-2:added powerStats property to hero interface

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,104 @@
 import { Hero } from 'src/core/interfaces/hero.interface';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  {
+    id: 11,
+    name: 'Dr Nice',
+    powerStats: {
+      combat: 5,
+      speed: 7,
+      strength: 5,
+      intelligence: 6,
+    },
+  },
+  {
+    id: 12,
+    name: 'Narco',
+    powerStats: {
+      combat: 6,
+      speed: 5,
+      strength: 2,
+      intelligence: 8,
+    },
+  },
+  {
+    id: 13,
+    name: 'Bombasto',
+    powerStats: {
+      combat: 6,
+      speed: 6,
+      strength: 6,
+      intelligence: 9,
+    },
+  },
+  {
+    id: 14,
+    name: 'Celeritas',
+    powerStats: {
+      combat: 7,
+      speed: 7,
+      strength: 3,
+      intelligence: 9,
+    },
+  },
+  {
+    id: 15,
+    name: 'Magneta',
+    powerStats: {
+      combat: 8,
+      speed: 3,
+      strength: 10,
+      intelligence: 3,
+    },
+  },
+  {
+    id: 16,
+    name: 'RubberMan',
+    powerStats: {
+      combat: 3,
+      speed: 8,
+      strength: 8,
+      intelligence: 4,
+    },
+  },
+  {
+    id: 17,
+    name: 'Dynama',
+    powerStats: {
+      combat: 5,
+      speed: 7,
+      strength: 5,
+      intelligence: 6,
+    },
+  },
+  {
+    id: 18,
+    name: 'Dr IQ',
+    powerStats: {
+      combat: 7,
+      speed: 4,
+      strength: 3,
+      intelligence: 5,
+    },
+  },
+  {
+    id: 19,
+    name: 'Magma',
+    powerStats: {
+      combat: 7,
+      speed: 5,
+      strength: 3,
+      intelligence: 9,
+    },
+  },
+  {
+    id: 20,
+    name: 'Tornado',
+    powerStats: {
+      combat: 4,
+      speed: 7,
+      strength: 3,
+      intelligence: 6,
+    },
+  },
 ];
